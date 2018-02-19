7fab4c2762d824a2054c0a2da1bb932f9d33309c
Loïc Denuzière <loic@denuziere.net>
2018-02-19 14:02:45 +0100

    Test setting build unstable

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
 echo %GIT_BRANCH%
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://intellifactory.com:8080/jnlpJars/jenkins-cli.jar', 'jenkins-cli.jar') }"
+java -jar jenkins-cli.jar set-build-result unstable
